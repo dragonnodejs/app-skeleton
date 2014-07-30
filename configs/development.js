e823624641d9003ca0aaf0847df34aca994d75d3
Christoph Herrmann <developer@dragonprojects.de>
2014-07-31 00:11:27 +0200

    Documentation for the configuration file

--- configs/development.js
+++ configs/development.js
@@ -1,6 +1,14 @@
 /**
  * Development configuration for the application server
- * @type {{libraries: {}, npm: string, directory: string, modules: {npm: {}, directory: {}}}}
+ * @type {
+ *      libraries: {},     // The libraries installed per NPM as "{ alias: name }"
+ *      npm: string,       // The path to "node_modules" directory used for the libraries and modules installed per NPM
+ *      directory: string, // The path to the directory with the modules of this project
+ *      modules: {
+ *          npm: {},       // The Modules installed per NPM with his configurations as "{ name: module }"
+ *          directory: {}  // The Modules of this project with his configurations as "{ name: module }"
+ *      }
+ * }
  */
 module.exports = {
     libraries: {},
