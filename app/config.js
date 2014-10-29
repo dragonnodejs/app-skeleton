41b24c88b36377ff6a2d334974f9a3f15a811989
Christoph Herrmann <developer@dragonprojects.de>
2014-10-30 00:34:46 +0100

    Formatting

--- app/config.js
+++ app/config.js
@@ -3,15 +3,15 @@
 /*
  * Configuration for the application
  * @type {
- *      npm: string,       // absolute path to "node_modules" directory for libraries and modules installed per NPM
+ *      npm: string,       // Absolute path to "node_modules" directory for libraries and modules installed per NPM
  *      libraries: {
- *          nodejs: {},    // libraries included in Node.js as "alias: name"
- *          npm: {}        // libraries installed per NPM as "alias: name"
+ *          nodejs: {},    // Libraries included in Node.js as "alias: name"
+ *          npm: {}        // Libraries installed per NPM as "alias: name"
  *      },
- *      directory: string, // absolute path to directory with the modules of this project
+ *      directory: string, // Absolute path to directory with the modules of this project
  *      modules: {
- *          npm: {},       // modules installed per NPM with their configuration as "name: config"
- *          directory: {}  // modules of this project with their configuration as "name: config"
+ *          npm: {},       // Modules installed per NPM with their configuration as "name: config"
+ *          directory: {}  // Modules of this project with their configuration as "name: config"
  *      }
  * }
  */
