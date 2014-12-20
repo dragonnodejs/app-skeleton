965a484174a576da3632a4ebe8a8e1c002fc60ef
Christoph Herrmann <developer@dragonprojects.de>
2014-12-20 14:38:13 +0100

    Refactor documentations

--- app/config.js
+++ app/config.js
@@ -1,26 +1,16 @@
 "use strict";
 
-/*
- * Configuration for the application
- * @type {
- *      npm: '',           // Absolute path to "node_modules" directory for NPM installed libraries and modules
- *      libraries: {
- *          nodejs: {},    // Node.js included libraries as "alias: name"
- *          npm: {}        // NPM installed libraries as "alias: name"
- *      },
- *      directory: '',     // Absolute path to directory with the application/bundle modules
- *      modules: {
- *          npm: {},       // NPM installed modules with the configuration as "name: config"
- *          directory: {}  // Modules of the directory with the configuration as "name: config"
- *      }
- * }
- */
+// Configuration for the application
 
 module.exports = {
     npm: __dirname + '/../node_modules/',
-    libraries: {},
+    libraries: {
+        nodejs: {},
+        npm: {}
+    },
     directory: __dirname + '/modules/',
     modules: {
+        npm: {},
         directory: {
             example: {
                 // Configuration for the module
