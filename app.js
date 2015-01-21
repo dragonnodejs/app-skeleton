6c45ea501d1a40340be605259c5335d27e483c0b
Christoph Herrmann <developer@dragonprojects.de>
2015-01-21 21:27:01 +0100

    Upgrade to DragonNode.js v3.1.0 and use the default values

--- app.js
+++ app.js
@@ -3,14 +3,8 @@
 // Load the libraries and modules
 
 var config = {
-    npm: __dirname + '/node_modules/',
-    libraries: {
-        nodejs: {},
-        npm: {}
-    },
     directory: __dirname + '/modules/',
     modules: {
-        npm: {},
         directory: {
             example: {
                 // Configuration for the module
